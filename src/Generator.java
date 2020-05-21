fa19357e4f54d5bb86fa35803264b3ffdf32df87
Nouw <offabio@outlook.com>
2020-05-21 13:49:52 +0200

    Added generator and main class

---
+++ src/Generator.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Generator {
+    private List<String> alphabet;
+    private List<String> random;
+
+    public Generator(int size) {
+        this.alphabet = Arrays.asList("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+//      TODO: Move this to own function/check if you can append directly to random.
+        List<String> tempList = new ArrayList<>();
+        for(int i = 0; i < size; i++) {
+            tempList.add(this.alphabet.get(ThreadLocalRandom.current().nextInt(0, 27)));
+        }
+        this.random = tempList;
+    }
+}
